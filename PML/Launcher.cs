a69459c08c4799a90498346df4c7c910e1f1e69f
ilonic23 <ilia.pwl@gmail.com>
2025-07-10 11:00:23 +0300

    Make it independent of installing the library

--- PML/Launcher.cs
+++ PML/Launcher.cs
@@ -2,7 +2,6 @@
 using CmlLib.Core;
 using CmlLib.Core.Auth;
 using CmlLib.Core.ProcessBuilder;
-using CmlLib.Core.Version;
 
 namespace PML;
 
@@ -17,28 +16,28 @@ public class Launcher
         _launcher = new (_path);
     }
 
-    public async Task<List<IVersion>> GetVersionsAsync(bool release = true, bool snapshot = true, bool old = true)
+    public async Task<List<string>> GetVersionsAsync(bool release = true, bool snapshot = true, bool old = true)
     {
-        List<IVersion> versions = new();
+        List<string> versions = new();
         foreach (var version in await _launcher.GetAllVersionsAsync())
         {
             if (release && version.Type == "release")
-                versions.Add(await _launcher.GetVersionAsync(version.Name));
+                versions.Add(version.Name);
             else if (snapshot && version.Type == "snapshot")
-                versions.Add(await _launcher.GetVersionAsync(version.Name));
+                versions.Add(version.Name);
             else if (old && version.Type == "old_alpha" || old && version.Type == "old_beta")
-                versions.Add(await _launcher.GetVersionAsync(version.Name));
+                versions.Add(version.Name);
         }
 
         return versions;
     }
-    public async Task InstallVersionAsync(IVersion version)
+    public async Task InstallVersionAsync(string version)
     {
         await _launcher.InstallAsync(version);
     }
-    public Process BuildProcess(IVersion version, MSession session)
+    public async Task<Process> BuildProcessAsync(string version, MSession session)
     {
-        return _launcher.BuildProcess(version, new MLaunchOption()
+        return await _launcher.BuildProcessAsync(version, new MLaunchOption()
         {
             Session = session
         });
