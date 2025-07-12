b3bb533a0cf72527cf3c85f34552ae00ae71550a
ilonic23 <ilia.pwl@gmail.com>
2025-07-12 18:00:28 +0300

    Add more options to BuildProcessAsync

--- PML/Launcher.cs
+++ PML/Launcher.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 using CmlLib.Core;
 using CmlLib.Core.Auth;
 using CmlLib.Core.ProcessBuilder;
@@ -7,13 +8,19 @@ namespace PML;
 
 public class Launcher
 {
-    private MinecraftLauncher _launcher;
-    private MinecraftPath _path;
+    private readonly MinecraftLauncher _launcher;
+
+    public string MinecraftNetherAuthFix { get; } =
+        "-Dminecraft.api.auth.host=https://nope.invalid -Dminecraft.api.account.host=https://nope.invalid -Dminecraft.api.session.host=https://nope.invalid -Dminecraft.api.services.host=https://nope.invalid";
 
     public Launcher(string path)
     {
-        _path = new PMLPath(path);
-        _launcher = new (_path);
+        _launcher = new (new PMLPath(path));
+    }
+
+    public bool ValidateNickname(string nickname)
+    {
+        return Regex.IsMatch(nickname, @"^[A-Za-z0-9_]{3,16}$");
     }
 
     public async Task<List<string>> GetVersionsAsync(bool release = true, bool snapshot = true, bool old = true)
@@ -37,9 +44,49 @@ public class Launcher
     }
     public async Task<Process> BuildProcessAsync(string version, MSession session)
     {
+        if (!ValidateNickname(session.Username!)) throw new ArgumentException("Invalid nickname.");
         return await _launcher.BuildProcessAsync(version, new MLaunchOption()
         {
             Session = session
+        });
+    }
+
+    public async Task<Process> BuildProcessAsync(string version, MSession session, int maximumRamMb,
+        bool fixNetherUpdateAuth)
+    {
+        if (!ValidateNickname(session.Username!)) throw new ArgumentException("Invalid nickname.");
+        return await _launcher.BuildProcessAsync(version, new MLaunchOption()
+        {
+            Session = session,
+            MaximumRamMb = maximumRamMb,
+            ExtraJvmArguments = ( fixNetherUpdateAuth && version.Contains("1.16") ) ?
+                new []
+                {
+                    MArgument.FromCommandLine(MinecraftNetherAuthFix),
+                }
+                : Array.Empty<MArgument>()
+        });
+    }
+
+    public async Task<Process> BuildProcessAsync(string version, MSession session, int maximumRamMb)
+    {
+        if (!ValidateNickname(session.Username!)) throw new ArgumentException("Invalid nickname.");
+        return await _launcher.BuildProcessAsync(version, new MLaunchOption()
+        {
+            Session = session,
+            MaximumRamMb = maximumRamMb
+        });
+    }
+    
+    public async Task<Process> BuildProcessAsync(string version, MSession session, int maximumRamMb, MArgument[] extraJvmArguments, MArgument[] extraGameArguments)
+    {
+        if (!ValidateNickname(session.Username!)) throw new ArgumentException("Invalid nickname.");
+        return await _launcher.BuildProcessAsync(version, new MLaunchOption()
+        {
+            Session = session,
+            MaximumRamMb = maximumRamMb,
+            ExtraJvmArguments = extraJvmArguments,
+            ExtraGameArguments = extraGameArguments
         });
     }
 }
